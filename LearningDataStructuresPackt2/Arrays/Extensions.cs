964bd5a11ef5e75f4edd20b431f3103d0fcda2b8
Zadok Joshua <ayegbazadok@outlook.com>
2022-05-10 16:15:58 +0100

    LinkedList

--- LearningDataStructuresPackt2/Arrays/Extensions.cs
+++ LearningDataStructuresPackt2/Arrays/Extensions.cs
@@ -3,10 +3,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static LearningDataStructuresPackt2.Arrays.ArrayList;
 
 namespace LearningDataStructuresPackt2.Arrays
 {
-    public class Extensions
-    {
-    }
+    public static class Extensions
+        {
+            public static void PrintElements<T>(this T[] array)
+            {
+                if (null == array || array.Length == 0)
+                {
+                    Console.WriteLine("[]");
+                    return;
+                }
+
+                Console.Write("[");
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        Console.Write($"{array[i]}");
+                    }
+                    else
+                    {
+                        Console.Write($", {array[i]}");
+                    }
+                }
+                Console.WriteLine("]");
+            }
+
+            public static void PrintElements<T>(this List<T> arrayList)
+            {
+                if (null == arrayList || arrayList.Count == 0)
+                {
+                    Console.WriteLine("[]");
+                    return;
+                }
+
+                Console.Write("[");
+                for (int i = 0; i < arrayList.Count; i++)
+                {
+                    if (i == 0)
+                    {
+                        Console.Write($"{arrayList[i]}");
+                    }
+                    else
+                    {
+                        Console.Write($", {arrayList[i]}");
+                    }
+                }
+                Console.WriteLine("]");
+            }
+
+            public static void PrintElements<T>(this ArrayList<T> arrayList)
+            {
+                if (null == arrayList || arrayList.Length == 0)
+                {
+                    Console.WriteLine("[]");
+                    return;
+                }
+
+                Console.Write("[");
+                for (int i = 0; i < arrayList.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        Console.Write($"{arrayList[i]}");
+                    }
+                    else
+                    {
+                        Console.Write($", {arrayList[i]}");
+                    }
+                }
+                Console.WriteLine($"], storageSize: {arrayList.StorageLength}");
+            }
+        }
 }
---
+++ LinkedList/Program.cs
@@ -0,0 +1,7 @@
+﻿using LinkedList;
+
+var head = new Node(7);
+head.next = new Node(3);
+head.next.next = new Node(1);
+head.next.next.next = new Node(2);
+head.next.next.next.next = new Node(3);
