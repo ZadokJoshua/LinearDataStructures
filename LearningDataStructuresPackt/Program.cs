6611e045ef7cc04f53e9f8859df1237c332643e4
Zadok Joshua <ayegbazadok@outlook.com>
2022-05-10 11:34:04 +0100

    Update

--- LearningDataStructuresPackt/Program.cs
+++ LearningDataStructuresPackt/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LearningDataStructuresPackt1;
+
+Rectangle a = new Rectangle(1, 2);
+Rectangle b = new Rectangle(3, 5);
+
+Console.WriteLine($"Rectangle a area: {a.GetArea()}");
+Console.WriteLine($"Rectangle b area: {b.GetArea()}");
+Console.WriteLine($"No. of angles in a rectangle is {Rectangle.GetNumberOfAngles()}");
