f95240ec01c190bc716683c5e3b4cb1f38dc522c
Zadok Joshua <ayegbazadok@outlook.com>
2022-05-10 12:30:14 +0100

    Update

--- LearningDataStructuresPackt/Program.cs
+++ LearningDataStructuresPackt/Program.cs
@@ -6,3 +6,9 @@ Rectangle b = new Rectangle(3, 5);
 Console.WriteLine($"Rectangle a area: {a.GetArea()}");
 Console.WriteLine($"Rectangle b area: {b.GetArea()}");
 Console.WriteLine($"No. of angles in a rectangle is {Rectangle.GetNumberOfAngles()}");
+
+Pair<int> pair = new Pair<int>(2, 6);
+Pair<string> pair1 = new Pair<string>("Zadok", "Joshua");
+Console.WriteLine($"First Pair: {pair.ReadPair}");
+
+Console.ReadLine();
