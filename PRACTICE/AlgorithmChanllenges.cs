fb8eab151e8454b39cecd674c47cf807f614e083
Zadok Joshua <ayegbazadok@outlook.com>
2022-05-13 14:42:14 +0100

    Strings Reversed

---
+++ PRACTICE/AlgorithmChanllenges.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAlgorithms
+{
+    public class AlgorithmChanllenges
+    {
+        public string theWord { get; set; }
+        // CHALLENGE 1: ALGORITHM THAT REVERSES EACH WORD IN A SENTENCE
+        public string ReverseEachWord(string word)
+        {
+            char[] arr = word.ToCharArray();
+            Array.Reverse(arr);
+            string sentenceRe = new String(arr);
+
+            char[] delimeter = { ' ' };
+            string[] newWords = sentenceRe.Split(delimeter);
+            string[] original = new string[newWords.Length];
+
+            int j = 0;
+            int len = newWords.Length - 1;
+
+            foreach(string wor in original)
+            {
+                original[j] = newWords[len];
+                j++;
+                len--;
+            }
+
+            return String.Join(" ", original);
+        }
+
+        
+    }
+}
---
+++ PRACTICE/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using CSharpAlgorithms;
+using System.Text;
+
+var stringsChallemnge = new AlgorithmChanllenges();
+Console.WriteLine(stringsChallemnge.ReverseEachWord("What can I do today"));
+
+
+
